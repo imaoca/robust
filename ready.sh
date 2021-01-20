9b66101533167acc86ef03d5a397d1d30c44400c
75u2u <75u2uuy4@gmail.com>
2021-01-20 11:41:29 +0900

    Update ready.sh

--- ready.sh
+++ ready.sh
@@ -3,7 +3,7 @@ rm data/*
 rm check.md5
 for i in `seq 0 999`
 do
-    cat /dev/urandom | base64 | head -c 102400 > data/data$i
+    cat /dev/urandom | head -c 102400 > data/data$i
 done
 cd data
 md5sum $(find . -type f) | tee ../check.md5
