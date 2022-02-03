c142a8d1b056359dd395be33da84584f39ea9ff1
michihiro imaoka <imaoca@gmail.com>
2022-02-03 17:41:08 +0900

    Create jammer.c

---
+++ jammer.c
@@ -0,0 +1,21 @@
+/*
+$ wget https://project-downloads.drogon.net/wiringpi-latest.deb
+$ sudo dpkg -i wiringpi-latest.deb
+$ gcc -Wall -o blink blink.c -lwiringPi
+*/
+
+#include <wiringPi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#define pin 7
+int main (void)
+{
+  wiringPiSetup () ;
+  pinMode (pin, OUTPUT) ;
+  for (;;)
+  {
+    digitalWrite (pin, HIGH) ; delay (15) ;
+    digitalWrite (pin,  LOW) ; delay (10+rand()%10) ;
+  }
+  return 0 ;
+}
